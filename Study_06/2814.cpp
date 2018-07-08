4d3cd0f3d52251f061c3147a6b7c363bc44e68b7
parkjieun <jieunpark247@daum.net>
2018-07-08 17:22:21 +0900

    add & modify '최장경로' using reculsive & 초기화init(**)

--- Study_06/2814.cpp
+++ Study_06/2814.cpp
@@ -1,12 +1,13 @@
-//fail 
+//2814 최장경로
+//초기화 조심
 #include<iostream>
 
 using namespace std;
 int n, m;
 int x, y;
 int cnt,result;
-int visited[12];
-int map[11][11];
+int visited[21];
+int map[21][21];
 
 void init_map() {
 	for (int i = 1; i <= n; i++)
@@ -20,14 +21,18 @@ void init_visited() {
 }
 
 void solve(int start) {
-	
+	if (result < cnt)
+		result = cnt;
 
 	for (int j = 1; j <= n; j++) {
-		if (map[start][j] == 1 && visited[j] != 1)
+		if (map[start][j] == 1 && visited[j] ==0)
 		{
 			visited[j] = 1;
 			cnt++;
 			solve(j);
+			cnt--;
+			visited[j] = 0;
+	
 		}		
 	}
 	
@@ -50,20 +55,17 @@ int main(){
 	
 
 		result = 0;
-		for (int i = 1; i <= n; i++)
-		{
 		
+		for (int i = 0; i < n; i++) {
 			cnt = 1;
 			init_visited();
 			visited[i] = 1;
 			solve(i);
-			if (result < cnt)
-				result = cnt;
-		
-			
 		}
+			
+		init_map();
 
 		cout << "#" << t << " "<< result<<endl;
 	}
-	return 0;
+
 }
