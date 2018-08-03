417686071f28eef04cbf292a0900930268ee9099
parkjieun <jieunpark247@daum.net>
2018-08-03 17:14:02 +0900

    not complete '지희의 고장난 계산기

---
+++ Study_07/1808.cpp
@@ -0,0 +1,32 @@
+//
+//  1808.cpp
+//  Project
+//
+//  지희의 고장난 계산기
+
+#include <iostream>
+
+using namespace std;
+int numSwitch[11];
+int numSum;
+int ans;
+int main(){
+    ans=-1; // 불가능 = -1
+    int testcase;
+    cin>>testcase;
+    for(int t=1;t<=testcase;t++){
+        
+        for(int i =1; i<=10;i++){
+            cin>>numSwitch[i];
+        }
+        cin>>numSum;
+    
+    
+    
+    
+    
+
+    cout<<"#" << t << " "<<ans<<endl;
+        
+    }
+}
