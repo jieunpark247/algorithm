ee1769ca2433e9d394f7742a43c4756a58e7c773
parkjieun <jieunpark247@daum.net>
2018-10-06 23:33:04 +0900

    add 로봇청소기 using if / recursion

--- Study_10/14503.cpp
+++ Study_10/14503.cpp
@@ -11,25 +11,12 @@ int map2[51][51];
 int ans;
 int dire[4] = { 3,0,1,2 };
 int back[4] = { 2,3,0,1 };
-void pr() {
-	cout << "\n'";
-	for (int i = 0; i < n; i++) {
-		cout << "\n'";
-			for (int j = 0; j < m; j++) {
-				cout << map[i][j];
-			}
-	}cout << "\n'";
-	cout << "\n'";
-}
 
 bool isRange(int x ,int y) {
 	return (x >= 0 && x < n &&y >= 0 && y < m); 
 }
 void robot(int x, int y, int dir,int cnt) {
 
-	//pr();
-	//cout << cnt<<'\n';
-	
 	if (map[x][y] == 0) //1¹ø
 		map[x][y] = 2;
 
@@ -37,69 +24,29 @@ void robot(int x, int y, int dir,int cnt) {
 	int nx = x + dx[leftdir];
 	int ny = y + dy[leftdir];
 	int b = back[dir];
-//	cout << map[nx][ny] << " sddf"<<x<<y << isRange(nx, ny)<<dir;
 
 	if (map[nx][ny] == 0 && isRange(nx, ny)) {
-		//cout << "A" << '\n';
-	//	cout << "dir" << leftdir << '\n';
+
 		cnt = 0;
 		robot(nx, ny, leftdir, cnt); 
 	}//2-1
 	else if (map[nx][ny] != 0 && cnt !=4 && isRange(nx, ny)) {
-	//	cout << "B" << '\n';
-	//	cout <<"dir"<< leftdir << '\n';
+
 		robot(x, y, leftdir, cnt + 1);
 	}//2-2
 	
 	else if (map[nx][ny] != 0 && cnt==4 && map[x + dx[b]][y + dy[b]] != 1 && isRange(nx, ny)) {
-		//cout << "C" << '\n';
-		//cout << "dir" << dir << '\n';
+	
 		cnt = 0;
 		robot(x + dx[b], y + dy[b], dir, cnt);
 	}//2-3
 	else if (map[nx][ny] != 0 && cnt ==4 && map[x + dx[b]][y + dy[b]] == 1 && isRange(nx, ny)) {
-		//cout << "D" << '\n';
-		//cout << "dir" << leftdir << '\n';
+		
 		return;
 	}//2-4
 
 	
 }
-/*void solve(int x, int y, int dir) {
-
-	int nx = x + dx[dir];
-	int ny = y + dy[dir];
-
-	switch (dir)
-	{
-		case 0:
-			robot();
-
-			solve(nx, ny, 3);
-			break;
-
-		case 1:
-
-			robot();
-			solve(nx, ny, 0);
-			break;
-
-		case 2:
-			robot();
-			solve(nx, ny, 1);
-			break; 
-
-		case 3:
-
-			robot();
-			solve(nx, ny, 2);
-			break;
-
-	}
-	
-
-
-}*/
 int main() {
 	cin >> n >> m;
 	cin >> r >> c >> d;
@@ -108,7 +55,6 @@ int main() {
 			cin >> map[i][j];
 	
 
-	// solve( r,  c,  d);
 	robot( r,c, d, 0);
 	 ans = 0;
 	 for (int i = 0; i < n; i++)
